9abc4c6cf573d63dabe7847bc9b8246b240ea643
Manas12Singh <rd511323@gmail.com>
2023-11-10 08:36:36 +0530

    started threadLib

---
+++ my_thread.c
@@ -0,0 +1,10 @@
+//
+// Created by Deepanshu Dhami on 10-11-2023.
+//
+
+#include "my_thread.h"
+
+void hello()
+{
+	printf("Hello World!\n");
+}
---
+++ my_thread.h
@@ -0,0 +1,14 @@
+//
+// Created by Deepanshu Dhami on 10-11-2023.
+//
+#ifndef STDIO_H
+#include "stdio.h"
+#endif
+
+
+#ifndef MY_THREAD_MY_THREAD_H
+#define MY_THREAD_MY_THREAD_H
+
+void hello();
+
+#endif //MY_THREAD_MY_THREAD_H
